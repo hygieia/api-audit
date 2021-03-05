254d3eb7c610863bb09f7573eafa8563a252804c
nireeshT <nireesh.thiruveedula@capitalone.com>
2021-03-05 13:40:17 -0500

    Autodiscovery audit type

--- src/main/java/com/capitalone/dashboard/request/DashboardAuditRequest.java
+++ src/main/java/com/capitalone/dashboard/request/DashboardAuditRequest.java
@@ -13,7 +13,7 @@ public class DashboardAuditRequest extends AuditReviewRequest {
     private String businessApplication;
     @ApiModelProperty(value = "Audit Type has one of these values: ALL, CODE_REVIEW, BUILD_REVIEW, CODE_QUALITY, TEST_RESULT, PERF_TEST,ARTIFACT", example = "ALL")
 	private Set<AuditType> auditType;
-    @ApiModelProperty(value = "Audit Type has one of these values: ALL, CODE_REVIEW, BUILD_REVIEW, CODE_QUALITY, TEST_RESULT, PERF_TEST,ARTIFACT", example = "ALL")
+    @ApiModelProperty(value = "Auto discover audit type has one of these values: ALL, CODE_REVIEW, BUILD_REVIEW, CODE_QUALITY, TEST_RESULT, PERF_TEST,ARTIFACT", example = "ALL")
     private AutoDiscoverAuditType autoDiscoverAuditType;
 
     private ArtifactAuditRequest artifactAuditRequest;
