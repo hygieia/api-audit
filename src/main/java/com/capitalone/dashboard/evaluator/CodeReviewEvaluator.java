54be3333351ac424e01f07eee798d53528d2766f
Christine Shen <christine.shen@capitalone.com>
2021-03-09 13:08:31 -0500

    extract merge commit from target branch regex to properties

--- src/main/java/com/capitalone/dashboard/evaluator/CodeReviewEvaluator.java
+++ src/main/java/com/capitalone/dashboard/evaluator/CodeReviewEvaluator.java
@@ -362,10 +362,14 @@ public class CodeReviewEvaluator extends Evaluator<CodeReviewAuditResponseV2> {
     private boolean isMergeCommitFromTargetBranch(Commit commit, GitRequest pr) {
         if(commit == null || pr == null) return false;
         String commitLog = commit.getScmCommitLog();
-        Pattern pattern = Pattern.compile("(.*merge branch.*\')(.*)(\'.*into.*)", Pattern.CASE_INSENSITIVE);
-        Matcher matcher = pattern.matcher(commitLog);
-        if(!matcher.matches()) return false;
-        return StringUtils.equalsIgnoreCase(pr.getScmBranch(), matcher.group(2));
+        List<String> mergeCommitFromTargetBranchRegEx = settings.getMergeCommitFromTargetBranchRegEx();
+        for (String mergeCommitRegex : mergeCommitFromTargetBranchRegEx) {
+            Pattern pattern = Pattern.compile(mergeCommitRegex, Pattern.CASE_INSENSITIVE);
+            Matcher matcher = pattern.matcher(commitLog);
+            if (matcher.matches()) return StringUtils.equalsIgnoreCase(pr.getScmBranch(), matcher.group(2)) // matches "merge branch 'x' into y" or "merge branch 'origin/x' into y
+                    || StringUtils.equalsIgnoreCase(pr.getScmUrl(), matcher.group(2)); // matches "merge github.com/org/repo into y"
+        }
+        return false;
     }
 
     protected boolean existsApprovedPROnAnotherBranch(CollectorItem repoItem, Commit commit, List<CollectorItem> collectorItemList,
