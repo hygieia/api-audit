40e3ac91d606ef51e32b28d2bbf0853d4628ddde
nameisaravind <nameisaravind@gmail.com>
2021-08-12 11:55:44 -0400

    else if added

--- src/main/java/com/capitalone/dashboard/evaluator/Evaluator.java
+++ src/main/java/com/capitalone/dashboard/evaluator/Evaluator.java
@@ -86,7 +86,7 @@ public abstract class Evaluator<T> {
             List<ObjectId> collectorItemIds = componentOpt.map(component ->
                     component.getCollectorItems(collectorType).stream().filter(c -> isEqualsAltIdentifierAndIdentifierName(c, altIdentifier, identifierName)).map(CollectorItem::getId).collect(Collectors.toList())).orElse(Collections.emptyList());
             return CollectionUtils.isNotEmpty(collectorItemIds) ? IterableUtils.toList(collectorItemRepository.findAll(collectorItemIds)) : getCollectorItemsByAltIdentifier(dashboard, collectorType, altIdentifier);
-        } if (StringUtils.isNotEmpty(altIdentifier)) {
+        } else if (StringUtils.isNotEmpty(altIdentifier)) {
             return getCollectorItemsByAltIdentifier(dashboard, collectorType, altIdentifier);
         } else {
             return getCollectorItems(dashboard,collectorType);
