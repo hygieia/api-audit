a6b690ec4956cd89afd2c37d174c988ee3d89492
nameisaravind <nameisaravind@gmail.com>
2022-03-28 14:39:38 -0400

    Revert "Audit based on altIdentifier" (#129)

--- src/main/java/com/capitalone/dashboard/util/ConversionUtils.java
+++ src/main/java/com/capitalone/dashboard/util/ConversionUtils.java
@@ -1,18 +1,11 @@
 package com.capitalone.dashboard.util;
 
-import com.capitalone.dashboard.model.CollectorItem;
-import com.capitalone.dashboard.model.GitComponent;
 import org.apache.commons.collections4.MapUtils;
 
 import java.util.Map;
-import java.util.Objects;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
 public class ConversionUtils {
-
-    public static final String TREE = "(.*\\/)(.*\\/)(.*\\/)(tree\\/)(.*)";
 
     public static final String flattenMap(Map<String, String[]> input) {
         return flattenMap(input, "", "");
@@ -28,26 +21,5 @@ public class ConversionUtils {
     private static final String arrayToString(String[] input) {
         if (input == null || input.length == 0) return "";
         return String.join("-", input);
-    }
-
-    public static boolean matchAltIdentifier(CollectorItem collectorItem, String altIdentifier) {
-        if (Objects.isNull(collectorItem.getAltIdentifier())) return false;
-        if(Objects.isNull(altIdentifier)) return false;
-        return getGitComponent(altIdentifier).equals(getGitComponent(collectorItem.getAltIdentifier()));
-    }
-
-    private static GitComponent getGitComponent(String altIdentifier) {
-        try{
-            Pattern pattern = Pattern.compile(TREE);
-            Matcher matcher = pattern.matcher(altIdentifier);
-            matcher.matches();
-            String org = matcher.group(2).replace("/", "");
-            String repo = matcher.group(3).replace("/", "");
-            String branch = matcher.group(5).replace("/", "");
-            return new GitComponent(org, repo, branch);
-        }catch (IllegalStateException e){
-            return new GitComponent("","","");
-        }
-
     }
 }
