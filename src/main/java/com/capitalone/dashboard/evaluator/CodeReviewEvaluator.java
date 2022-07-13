298a76bec1c089cc729c0449871900605439b6e6
dcanar9 <anthony.canar@hotmail.com>
2022-07-13 13:17:14 -0700

    removing star import

--- src/main/java/com/capitalone/dashboard/evaluator/CodeReviewEvaluator.java
+++ src/main/java/com/capitalone/dashboard/evaluator/CodeReviewEvaluator.java
@@ -31,7 +31,15 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+import java.util.Optional;
+import java.util.Set;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
