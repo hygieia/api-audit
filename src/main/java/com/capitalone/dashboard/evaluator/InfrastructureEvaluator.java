dede8b636bd86360631526294f714b72fedf7aa7
dcanar9 <anthony.canar@hotmail.com>
2022-11-29 17:12:26 -0700

    conflict

--- src/main/java/com/capitalone/dashboard/evaluator/InfrastructureEvaluator.java
+++ src/main/java/com/capitalone/dashboard/evaluator/InfrastructureEvaluator.java
@@ -71,6 +71,7 @@ public class InfrastructureEvaluator extends Evaluator<InfrastructureAuditRespon
         infrastructureAuditResponse.setAuditEntity(collectorItem.getOptions());
         infrastructureAuditResponse.setLastUpdated(collectorItem.getLastUpdated());
 
+
         List<InfrastructureScan> infrastructureScans = infrastructureScanRepository.findByCollectorItemIdAndTimestampIsBetweenOrderByTimestampDesc(collectorItem.getId(), beginDate - 1, endDate + 1);
         List<InfrastructureScan> filteredForBAP = StringUtils.isEmpty(businessComponent) ? Collections.EMPTY_LIST :
                 infrastructureScans.stream().filter(infrastructureScan -> infrastructureScan.getBusinessApplication().equalsIgnoreCase(businessComponent)).collect(Collectors.toList());
